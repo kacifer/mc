01faa87167b0ce9373f48ad92424fd7f390e9e11
JC <junchawu@tesla.com>
2023-03-24 10:46:51 -0700

    Define ErrorDetails

--- mgin/error.go
+++ mgin/error.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 )
 
+type ErrorDetails map[string]any
+
 type E struct {
 	// Code is the error code, if not specified, it will be the same as the http status code.
 	Code int `json:"code"`
@@ -13,7 +15,7 @@ type E struct {
 	Message string `json:"message"`
 
 	// Details can be used to provide more details about the error, such as the invalid form fields.
-	Details map[string]any `json:"details,omitempty"`
+	Details ErrorDetails `json:"details,omitempty"`
 }
 
 func (e *E) Error() string {
